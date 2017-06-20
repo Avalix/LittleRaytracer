83824c975238ffc449d6b7803ccbfa52d5fc6d8f
Chris Butler <chrisbutler@polygonhearts.com>
2017-06-20 15:24:39 +0100

    Inital VS commit

---
+++ Source/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+
+void main()
+{
+	std::cout << "Little Raytracer - Chris Butler(@avalix)\n\n";
+
+	std::cout << "DONE!\n";
+	std::cout << "Press any key to continue...";
+	getchar();
+}
